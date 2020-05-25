1bef4c80d223da022be3c23e68313bb7e1792baa
albuzi <b-alexandr@mail.ru>
2020-05-25 20:31:37 +0300

    Updates

---
+++ precache-manifest.2b632399f05d88f3b2f471bf29986b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf699ad053232bf3355ba07d0a0098e6",
+    "url": "/telegram_app/index.html"
+  },
+  {
+    "revision": "193561c25c0ab2ef2f4f",
+    "url": "/telegram_app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a27e2e1f96477eb1489b",
+    "url": "/telegram_app/static/css/main.3c11f4d0.chunk.css"
+  },
+  {
+    "revision": "193561c25c0ab2ef2f4f",
+    "url": "/telegram_app/static/js/2.c8526ef9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/telegram_app/static/js/2.c8526ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27e2e1f96477eb1489b",
+    "url": "/telegram_app/static/js/main.7b226bc8.chunk.js"
+  },
+  {
+    "revision": "5199cdff3f48d4ad39d0",
+    "url": "/telegram_app/static/js/runtime-main.4201f80d.js"
+  }
+]);
